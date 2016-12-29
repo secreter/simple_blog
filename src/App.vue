c461482e4e30f1825009253aeb83e86960f482f1
So <1979510177@qq.com>
2016-12-29 18:57:48 +0800

    use vue-router and postcss plugin postcss-cssnext

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+body{
+  background-color: var(--primary-bg-color);
+}
+
+</style>
